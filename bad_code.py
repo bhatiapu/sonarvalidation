ac510ab661e0859aa542728563bde8c3f4c488e3
Puneet Bhatia <puneet.bhatiau@gmail.com>
2025-12-29 19:20:04 -0500

    commiit

--- bad_code.py
+++ bad_code.py
@@ -17,6 +17,7 @@ def duplicate_code_1():
     print("Processing data")
     print("Processing data")
     print("Processing data")
+    print("Processing data1")
     result = []
     for i in range(10):
         result.append(i * 2)
